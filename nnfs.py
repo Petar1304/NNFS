1c5b68e8680033fe4b3ca1f694d4f468f9e32cba
Petar1304 <kovacevic1304@gmail.com>
2022-04-04 12:41:39 +0200

    Cleaned

--- nnfs.py
+++ nnfs.py
@@ -12,6 +12,7 @@ X = np.array([[1, 2, 3, 2.5],
               [-1.5, 2.7, 3.3, -0.8]])
 
 
+# CLASSES FOR NEURAL NETWORK
 class Layer_Dense:
   def __init__(self, n_inputs, n_neurons):
     self.weights = 0.1 * np.random.rand(n_inputs, n_neurons) # transpose already done with changing order of n_inputs and n_neurons
@@ -26,6 +27,7 @@ class Activation_ReLU:
   def forward(self, inputs):
     self.output = np.maximum(0, inputs)
 
+
 # SOFTMAX
 class Activation_Softmax:
   def forward(self, inputs):
@@ -33,12 +35,14 @@ class Activation_Softmax:
     probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
     self.output = probabilities
 
+
 # LOSS
 class Loss:
   def calculate(self, output, y):
     sample_losses = self.forward(output, y)
     data_loss = np.mean(sample_losses)
     return data_loss
+
 
 class Loss_CategoricalCrossEntropy(Loss):
   def forward(self, y_pred, y_true):
@@ -55,29 +59,27 @@ class Loss_CategoricalCrossEntropy(Loss):
     return negative_log_likelihoods
 
 
-X, y = spiral_data(samples=10, classes=3)
-activation1 = Activation_ReLU()
- 
-# layer1 = Layer_Dense(2, 10) # first parameter is how many inputs do we have, second could be anything
-# layer2 = Layer_Dense(10, 2) # output from layer1 is input, output could be anything
 
+if __name__ == '__main__':
+  # data
+  X, y = spiral_data(samples=10, classes=3)
+  activation1 = Activation_ReLU()
+  
+  # layer1 = Layer_Dense(2, 10) # first parameter is how many inputs do we have, second could be anything
+  # layer2 = Layer_Dense(10, 2) # output from layer1 is input, output could be anything
 
-dense1 = Layer_Dense(2, 3)
-activation1 = Activation_ReLU()
-dense2 = Layer_Dense(3, 3)
-activation2 = Activation_Softmax()
+  dense1 = Layer_Dense(2, 3)
+  activation1 = Activation_ReLU()
+  dense2 = Layer_Dense(3, 3)
+  activation2 = Activation_Softmax()
 
-dense1.forward(X)
-activation1.forward(dense1.output)
-dense2.forward(activation1.output)
-activation2.forward(dense2.output)
-print(activation2.output)
+  dense1.forward(X)
+  activation1.forward(dense1.output)
+  dense2.forward(activation1.output)
+  activation2.forward(dense2.output)
 
-loss_function = Loss_CategoricalCrossEntropy()
-loss = loss_function.calculate(activation2.output, y)
+  loss_function = Loss_CategoricalCrossEntropy()
+  loss = loss_function.calculate(activation2.output, y)
 
-print('Loss: ', loss)
-
-
-
+  print('Loss: ', loss)
 
